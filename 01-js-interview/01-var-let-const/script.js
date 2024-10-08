a4b6724a8ec62ba655f8487f1000373db9d77c68
gitdeepaks <deepaksankhyan92@outlook.com>
2024-10-08 19:53:58 +0530

    initiated

---
+++ 01-js-interview/01-var-let-const/script.js
@@ -0,0 +1,34 @@
+// function test() {
+//   var a = "Hello";
+//   let b = "Bye";
+
+//   if (true) {
+//     let a = "Hi";
+//     var b = "GoodBye";
+
+//     console.log(a);
+//     console.log(b);
+//   }
+// }
+
+var a;
+var a;
+
+let b;
+
+{
+  let b;
+}
+
+//Declaration without initialization
+
+const a = 4;
+
+var a = 6;
+
+a = 7;
+
+let a = 8;
+a = 9;
+
+// Hoisting
