72ead60ca8792857c8384990bfb49fc88a21e2c0
gitdeepaks <deepaksankhyan92@outlook.com>
2024-10-30 17:10:13 +0530

    functions scope

--- 01-js-interview/03-functions/script.js
+++ 01-js-interview/03-functions/script.js
@@ -2,3 +2,37 @@
 function square(num) {
   return num * num;
 }
+
+// function expression
+
+const sqr = function (num) {
+  return num + num;
+};
+
+// console.log(sqr(7));
+
+function displaySquare(fn) {
+  console.log("Sqr is" + fn(5));
+}
+
+displaySquare(sqr);
+
+// IIFFE
+
+(function add(num) {
+  return num + num;
+})(7);
+
+(function (x) {
+  return (function (y) {
+    console.log(x);
+  })(3);
+})(1);
+
+// function Scope  - O/P based Questions
+
+for (let i = 0; i < 5; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, i * 1000);
+}
