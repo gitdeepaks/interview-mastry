da564471049659a4379afab88424ae049b0f71a5
gitdeepaks <deepaksankhyan92@outlook.com>
2024-11-09 20:23:21 +0530

    learned event loop

--- 01-js-interview/15-event-loop/index.js
+++ 01-js-interview/15-event-loop/index.js
@@ -1,16 +1,28 @@
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((res) => res.json())
-  .then((res) => {
-    console.dirxml("fetch completed", res[0]);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => res.json())
+//   .then((res) => {
+//     console.dirxml("fetch completed", res[0]);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+// Promise.resolve().then(() => {
+//   console.dir("Promise resolved");
+// });
+
+// Promise.reject().catch(() => {
+//   console.dir("Promise.rejected");
+// });
+
+console.log("start");
+
+setTimeout(() => {
+  console.log("task queue");
+}, 0);
 
 Promise.resolve().then(() => {
-  console.dir("Promise resolved");
+  console.log("micro task queue Promise");
 });
 
-Promise.reject().catch(() => {
-  console.dir("Promise.rejected");
-});
+console.log("end");
