5f2409a160cfcf2dfa15eac25195a5ea5c48f3ab
gitdeepaks <deepaksankhyan92@outlook.com>
2024-11-30 09:21:40 +0530

    calCulateTotalSales

--- 03-DSA/2-Higher-Order-Array-Methods/calCulateTotalSales.js
+++ 03-DSA/2-Higher-Order-Array-Methods/calCulateTotalSales.js
@@ -1,6 +1,11 @@
 function calculateTotalSales(products, taxRate) {
-  const taotalSales = products.reduce(
+  const totalSales = products.reduce(
     (acc, prd) => acc + prd.price * prd.quantity,
     0
   );
+  const taxAmount = (totalSales * taxRate) / 100;
+
+  const totalSalesWithTax = totalSales + taxAmount;
+
+  return parseFloat(totalSalesWithTax.toFixed(2));
 }
