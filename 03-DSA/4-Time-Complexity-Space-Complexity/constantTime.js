d92421352b5186f5df278d3bae432bb33f8676a3
gitdeepaks <deepaksankhyan92@outlook.com>
2024-12-31 21:24:29 +0530

    learn some time complexity stuff

--- 03-DSA/4-Time-Complexity-Space-Complexity/constantTime.js
+++ 03-DSA/4-Time-Complexity-Space-Complexity/constantTime.js
@@ -7,15 +7,11 @@ function accessElement(arr, index) {
 const arr1 = [1, 2, 3, 4, 5];
 
 console.time("accessElement1");
-
 console.log(accessElement(arr1, 1));
-
 console.timeEnd("accessElement1");
 
 const arr2 = Array.from({ length: 10000 }, (_, index) => index + 1);
 
 console.time("accessElement2");
-
 console.log(accessElement(arr2, 100));
-
 console.timeEnd("accessElement2");
