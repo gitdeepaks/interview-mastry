b088ae596a9b60ba31c0f68d6124761575213d3b
gitdeepaks <deepaksankhyan92@outlook.com>
2025-01-03 15:54:57 +0530

    learned stacks basics

---
+++ 03-DSA/6-stack-queues-Liked-list/myStack.js
@@ -0,0 +1,53 @@
+class Stack {
+  constructor() {
+    this.maxSize = 100;
+    this.stack = [];
+    this.top = -1;
+  }
+
+  push(value) {
+    if (this.isFull()) {
+      return false;
+    }
+
+    this.top++;
+    this.stack[this.top] = value;
+    return true;
+  }
+
+  isFull() {
+    return this.top === this.maxSize - 1;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const valueToReturn = this.stack[this.top];
+
+    this.top--;
+    return this.stack.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    return this.stack[this.top];
+  }
+
+  isEmpty() {
+    return this.top === -1;
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log(stack.pop());
+console.log(stack.peek());
+
+// console.log(stack);
