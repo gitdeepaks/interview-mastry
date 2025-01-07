030a18a5f9098d6998d0ecebdcc7ad5638c717d5
gitdeepaks <deepaksankhyan92@outlook.com>
2025-01-07 20:50:27 +0530

    practicing sets

---
+++ index.js
@@ -0,0 +1,16 @@
+// const user = {
+//   firstName: "Piyush!",
+//   getName() {
+//     const firstName = "Piyush!";
+//     return this.firstName;
+//   },
+// };
+
+// console.log(object.getMessage());
+
+const set = new Set([3, 2, 1, 1, 2]);
+console.log(set);
+
+set.add(1);
+set.add(2);
+set.add(1);
