2c801280acf4ec3f583b77e5c6f975f7b10b0d9a
gitdeepaks <deepaksankhyan92@outlook.com>
2025-01-17 21:38:23 +0530

    practiced some concept of foreach loop

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-// const user = {
-//   firstName: "Piyush!",
-//   getName() {
-//     const firstName = "Piyush!";
-//     return this.firstName;
-//   },
-// };
+// // const user = {
+// //   firstName: "Piyush!",
+// //   getName() {
+// //     const firstName = "Piyush!";
+// //     return this.firstName;
+// //   },
+// // };
 
-// console.log(object.getMessage());
+// // console.log(object.getMessage());
 
-const set = new Set([3, 2, 1, 1, 2]);
-console.log(set);
+// const set = new Set([3, 2, 1, 1, 2]);
+// console.log(set);
 
-set.add(1);
-set.add(2);
-set.add(1);
+// set.add(1);
+// set.add(2);
+// set.add(1);
+
+const newArr = ["deepak", "vignesh", "saif"];
+
+for (const arr of newArr) {
+  console.log(arr + "-surname");
+}
+
+const mySurName = newArr.map((arr) => arr + "-mapsurname");
+
+console.log(mySurName);
+
+const surname = "SurnameForEach";
+
+let updatedArr = [];
+newArr.forEach((name) => {
+  updatedArr.push(`${name} ${surname}`);
+});
+
+console.log(updatedArr);
