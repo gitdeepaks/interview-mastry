23688c851f1970ad65cbe4ba9657d4ca8f58b631
gitdeepaks <deepaksankhyan92@outlook.com>
2025-01-31 20:03:18 +0530

    pratice implementation of Linkedlist

--- index.ts
+++ index.ts
@@ -65,3 +65,27 @@ const run = (message: string) => {
 };
 
 run("hello");
+import { expect, it } from "vitest";
+
+const handleFormData = (e: any) => {
+  e.preventDefault();
+  const data = new FormData(e.terget);
+  const value = Object.fromEntries(data.entries());
+  return value;
+};
+
+it("Should handle a form submit", () => {
+  const form = document.createElement("form");
+  form.innerHTML = `
+    <input name="name" value="John Doe" />
+  `;
+
+  form.onsubmit = (e) => {
+    const value = handleFormData(e);
+    expect(value).toEqual({ name: "John Doe" });
+  };
+
+  form.requestSubmit();
+
+  expect.assertions(1);
+});
