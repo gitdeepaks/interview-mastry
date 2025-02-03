77c282401963d17891e499edd2603afc2d2868de
gitdeepaks <deepaksankhyan92@outlook.com>
2025-02-03 12:28:25 +0530

    pratice ts

--- index.ts
+++ index.ts
@@ -108,3 +108,21 @@ const func = () => {
 function getRandonPercentage() {
   return `${(Math.random() * 100).toFixed(2)}%`;
 }
+
+const getResolvedIconSize = (
+  iconSize: "small" | "medium" | "large" | string
+) => {
+  switch (iconSize) {
+    case "small":
+      return 16;
+    case "medium":
+      return 32;
+    case "large":
+      return 48;
+    default:
+      return iconSize;
+  }
+};
+
+// It doesn't give you autocomplete for 'small', 'medium', or 'large'!
+getResolvedIconSize("awdawd");
