16c5512992a095c97410c7d005cd4c36ffee58d1
gitdeepaks <deepaksankhyan92@outlook.com>
2025-02-04 17:07:26 +0530

    pratice ts

--- index.ts
+++ index.ts
@@ -126,3 +126,36 @@ const getResolvedIconSize = (
 
 // It doesn't give you autocomplete for 'small', 'medium', or 'large'!
 getResolvedIconSize("awdawd");
+
+// 1. CON: Function return types can be wider than
+// what is actually returned
+
+import { Equal, Expect } from "@total-typescript/helpers";
+
+const returnsStringOrNumber = () => {
+  return 1;
+};
+
+const value = returnsStringOrNumber();
+
+if (typeof value === "string") {
+  type test = Expect<Equal<typeof value, never>>;
+
+  // @ts-expect-error
+  value.toUpperCase();
+}
+
+// 2. PRO: Function return types can help enforce the type of the function
+
+type UserRole = "admin" | "editor" | "viewer";
+
+function getPermissions(role: UserRole): string[] {
+  switch (role) {
+    case "admin":
+      return ["create", "read", "update", "delete"];
+    case "editor":
+      return ["create", "read", "update"];
+    case "viewer":
+      return ["read"];
+  }
+}
