8c8fa2e32ec02159249389718c132f79809b17de
gitdeepaks <deepaksankhyan92@outlook.com>
2025-02-13 20:28:23 +0530

    practised objects in typescript

--- index.ts
+++ index.ts
@@ -159,3 +159,42 @@ function getPermissions(role: UserRole): string[] {
       return ["read"];
   }
 }
+
+type User = {
+  id: string;
+  createdAt: Date;
+  name: string;
+  email: string;
+};
+
+type Product = {
+  id: string;
+  createdAt: Date;
+  name: string;
+  price: number;
+};
+
+type tests = [
+  Expect<
+    Extends<
+      {
+        id: string;
+        createdAt: Date;
+        name: string;
+        email: string;
+      },
+      User
+    >
+  >,
+  Expect<
+    Extends<
+      {
+        id: string;
+        createdAt: Date;
+        name: string;
+        price: number;
+      },
+      Product
+    >
+  >
+];
