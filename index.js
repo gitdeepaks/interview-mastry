a8776b393ce12e6d903c5321649d515304a30be4
gitdeepaks <deepaksankhyan92@outlook.com>
2025-02-16 21:00:34 +0530

    practiced som js problems

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-// // const user = {
-// //   firstName: "Piyush!",
-// //   getName() {
-// //     const firstName = "Piyush!";
-// //     return this.firstName;
-// //   },
-// // };
+// // // const user = {
+// // //   firstName: "Piyush!",
+// // //   getName() {
+// // //     const firstName = "Piyush!";
+// // //     return this.firstName;
+// // //   },
+// // // };
 
-// // console.log(object.getMessage());
+// // // console.log(object.getMessage());
 
-// const set = new Set([3, 2, 1, 1, 2]);
-// console.log(set);
+// // const set = new Set([3, 2, 1, 1, 2]);
+// // console.log(set);
 
-// set.add(1);
-// set.add(2);
-// set.add(1);
+// // set.add(1);
+// // set.add(2);
+// // set.add(1);
 
-const newArr = ["deepak", "vignesh", "saif"];
+// const newArr = ["deepak", "vignesh", "saif"];
 
-for (const arr of newArr) {
-  console.log(arr + "-surname");
-}
+// for (const arr of newArr) {
+//   console.log(arr + "-surname");
+// }
 
-const mySurName = newArr.map((arr) => arr + "-mapsurname");
+// const mySurName = newArr.map((arr) => arr + "-mapsurname");
 
-console.log(mySurName);
+// console.log(mySurName);
 
-const surname = "SurnameForEach";
+// const surname = "SurnameForEach";
 
-let updatedArr = [];
-newArr.forEach((name) => {
-  updatedArr.push(`${name} ${surname}`);
-});
+// let updatedArr = [];
+// newArr.forEach((name) => {
+//   updatedArr.push(`${name} ${surname}`);
+// });
 
-console.log(updatedArr);
+// console.log(updatedArr);
 
 function sum(...nums) {
   return nums.reduce((acc, currVal) => acc + currVal, 0);
@@ -49,5 +49,42 @@ run("hello");
 function add(counter) {
   return function anotherSun(nums) {
     counter++;
+  };
+}
+
+Function.prototype.des = function () {
+  console.log(`description ${this.name}`);
+};
+
+function greet() {
+  return `Hello, ${this.name}!`;
+}
+
+greet.des();
+
+greet.des("deepak");
+
+function add(a, b) {
+  return a + b;
+}
+
+const subs = function (a, b) {
+  return a - b;
+};
+
+const multiply = (a, b) => a * b;
+
+function applyOne(a, b, operation) {
+  return operation(a, b);
+}
+
+const res = applyOne(2, 3, (a, b) => a + b);
+
+function createCounter() {
+  let count = 0;
+
+  return function () {
+    count++;
+    return count;
   };
 }
